8b45136c76f8d1ac35ae8f0f7730c9d578cf6f36
sbv <38765146+stephanBV@users.noreply.github.com>
2021-09-17 12:17:59 +0200

    added name to chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,22 +15,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         #accept the connecion
         await self.accept()
 
-        #send messages into group chat
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'hello world',
-            }
-        )
-    #send message via websocket
-    async def tester_message(self, event):
-        tester = event['tester'] # the event
-
-        await self.send(text_data=json.dumps({
-            'tester': tester
-        }))
-
 
     #disconnect
     async def disconnect(self, close_code):
@@ -42,20 +26,24 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username' : username
             }
         )
 
     async def chatroom_message(self, event):
         message = event['message']
+        username = event['username']
 
         await self.send(text_data=json.dumps({
             'message': message,
+            'username' : username,
         }))
     
     pass
